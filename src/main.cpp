fac61231bed07e2a0f5bd2c2d279e8bf76c7ada6
Edgar Modesto <edgarmodesto23@gmail.com>
2025-12-03 15:33:59 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -55,9 +55,11 @@ int main(int argc, char **argv) {
 
   // Uncomment the code below to pass the first stage
   //
-  accept(server_fd, (struct sockaddr *)&client_addr,
-         (socklen_t *)&client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                         (socklen_t *)&client_addr_len);
   std::cout << "Client connected\n";
+
+  write(client_fd, "+PONG\r\n", 7);
 
   close(server_fd);
 
