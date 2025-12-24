4a7e7fe6d5c6f785445dff29ba822a24106e5666
Edgar Modesto <edgarmodesto23@gmail.com>
2025-12-24 13:02:49 -0700

    codecrafters submit [skip ci]

--- src/commands/InfoCommand.cc
+++ src/commands/InfoCommand.cc
@@ -9,11 +9,11 @@ InfoCommand::InfoCommand(std::shared_ptr<Database> db, std::string &buf,
 void InfoCommand::serveRequest(std::function<void(std::string)> respond) {
   if (db_->config_.master_address == "master")
     respond(BulkString::to_resp(
-        "role:master\nmastereplid:"
+        "role:master\nmaster_replid:"
         "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb\nmaster_repl_offset:0"));
   else
     respond(BulkString::to_resp(
-        "role:slave\nmastereplid:"
+        "role:slave\nmaster_replid:"
         "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb\nmaster_repl_"
         "offset:0"));
 }
